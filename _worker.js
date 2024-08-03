729ca5e545a5ba23027eacf346bcf9a7caddaf7c
leeyes202311 <151860274+leeyes202311@users.noreply.github.com>
2024-08-03 11:15:28 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'a1cb1996-ecc3-44d3-b5f5-eefc898724b8';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
